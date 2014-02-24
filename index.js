8fa06f605e302539ab65a6825f5faea4eb3c6e53
OpenShift guest <gtdminh@gmail.com>
2014-02-24 03:20:38 -0500

    some changes

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-var http = require("http");
+var server = require("./lib/server");
 
-var server = http.createServer(function(request,response)
-{
-    console.log(request);
-    response.writeHead(200,{"Content-Type":"text/plain"});
-    response.write("Hello");
-    response.end();
-});
-
-server.listen(8888);
+server.start(8888);
